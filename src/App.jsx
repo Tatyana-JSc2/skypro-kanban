8fbe0354a5cdc9a534f6705e21f8e6df78f26006
Tatyana-JSc2 <tanya.tanechka912@gmail.com>
2024-03-28 20:45:25 +0300

    sk1

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PopExit from './components/PopExit/PopExit';
+import PopNewCard from './components/PopNewCard/PopNewCard';
+import PopBrowse from './components/PopBrowse/PopBrowse';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+
+function App() {
+
+  return (
+
+    <div className="wrapper">
+      {/* pop-up start*/}
+
+      <PopExit />
+
+      <PopNewCard />
+
+      <PopBrowse />
+
+      {/* pop-up end*/}
+
+      <Header />
+
+      <Main />
+
+    </div>
+
+  )
+}
+
+export default App
