e9064339df21a16b98b9de6cd40dfb00af3e9f52
Tatyana-JSc2 <tanya.tanechka912@gmail.com>
2024-04-17 17:08:36 +0300

    sk5 попытка регистрации

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -7,52 +7,45 @@ import * as S from "./Register.styled";
 
 
 function Register({ setIsAuth, setToken }) {
-	//const [formData, setFormData] = useState(formFields);
+	const [formData, setFormData] = useState(formFields);
 	const navigate = useNavigate();
 
-	//const formFields = {
-	//	firstName: "",
-	//	email: "",
-	//	password: "",
-	//};
+	const formFields = {
+		firstName: "",
+		email: "",
+		password: "",
+	};
 
-	//const handleInputChange = (e) => {
-	//	const { name, value } = e.target; // Извлекаем имя поля и его значение
+	const handleInputChange = (e) => {
+		const { name, value } = e.target; // Извлекаем имя поля и его значение
 
-	//	setFormData({
-	//		...formData, // Копируем текущие данные из состояния
-	//		[name]: value, // Обновляем нужное поле
-	//	});
-	//};
+		setFormData({
+			...formData, // Копируем текущие данные из состояния
+			[name]: value, // Обновляем нужное поле
+		});
+	};
 
 
-	//{
-	//	name: formData.firstName,
-	//	login: formData.email,
-	//	password: formData.password,
+	function reg() {
+		getReg().then((data) => {
+			//throw new Error("Ошибка сервера");
+			setToken(data.token);
+		}).catch((err) => {
+			setError(err.message);
+		}).finally(() => {
+			setIsLoading(false);
+			localStorage.setItem("user", "user");
+			setIsAuth(true);
+			navigate(Paths.MAIN);
+		});
+	};
+
+
+	//function Try() {
+	//	localStorage.setItem("user", "user");
+	//	setIsAuth(true);
+	//	navigate(Paths.MAIN);
 	//}
-
-
-	//function reg() {
-	//	getReg().then((data) => {
-	//throw new Error("Ошибка сервера");
-	//		setToken(data.token);
-	//	}).catch((err) => {
-	//		setError(err.message);
-	//	}).finally(() => {
-	//setIsLoading(false);
-	//		localStorage.setItem("user", "user");
-	//		setIsAuth(true);
-	//		navigate(Paths.MAIN);
-	//	});
-	//};
-
-
-	function Try() {
-		localStorage.setItem("user", "user");
-		setIsAuth(true);
-		navigate(Paths.MAIN);
-	}
 
 	//value={formData.firstName} onChange={handleInputChange} 
 	//value={formData.email} onChange={handleInputChange} 
@@ -67,10 +60,10 @@ function Register({ setIsAuth, setToken }) {
 							<h2>Регистрация</h2>
 						</S.ModalTtl>
 						<S.ModalFormLogin id="formLogUp" action="#">
-							<S.ModalInput className="first-name" type="text" placeholder="Имя" name="first-name" label="Имя" />
-							<S.ModalInput className="login" type="text" placeholder="Эл. почта" name="login" label="Эл. почта" />
-							<S.ModalInput className="password-first" type="password" placeholder="Пароль" name="password" label="Пароль" />
-							<S.ModalBtn id="SignUpEnter" onClick={Try}>Зарегистрироваться</S.ModalBtn>
+							<S.ModalInput className="first-name" type="text" placeholder="Имя" name="first-name" label="Имя" value={formData.firstName} onChange={handleInputChange} />
+							<S.ModalInput className="login" type="text" placeholder="Эл. почта" name="login" label="Эл. почта" value={formData.email} onChange={handleInputChange} />
+							<S.ModalInput className="password-first" type="password" placeholder="Пароль" name="password" label="Пароль" value={formData.password} onChange={handleInputChange} />
+							<S.ModalBtn id="SignUpEnter" onClick={reg}>Зарегистрироваться</S.ModalBtn>
 							<S.ModalFormGroup>
 								<p>Уже есть аккаунт?  <Link to={Paths.LOGIN}>Войдите здесь</Link>
 								</p>
